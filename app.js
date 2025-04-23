eadb51f2992a27e3d8b363284bd858d97501c785
Guido <guidopasciucco@gmail.com>
2025-04-23 11:37:25 -0300

    Clase 3

--- app.js
+++ app.js
@@ -1,136 +1,192 @@
-/* 
-JAVASCRIPT - CLASE 2 DW2 
-    - ✅ VINCULACIÓN HTML con JS
-    - ✅ VARIABLES Y CONSTANTES
-    - ✅ TIPOS DE DATOS
-    - ✅ CONCATENACIÓN DE VARIABLES y Strings (+)
-    - ✅ Plantillas literales (si o si comilla invertidas)
-    - ✅ SUMA (+) Y RESTA (-) DE VARIABLES
-    - ✅ CURIOSIDADES / COMPORTAMIENTOS DE JS
-    - ✅ SINTAXIS BÁSICA
-        - let variables - declarar y asignar tanto junto como separado
-        - const constantes - siempre hacemos los 2 pasos juntos
-        - console.log() método de js para visualizar cosas en la consola
-        - = - operador de asignación - asigno dato a una variable o constante
-        - op. + suma o concat dependiendo de los datos que maneja
-        - sintaxis plantillas literales - backticks y ${}
+/*
+
+
+CLASE 3 
+- ✅ Prompt y Alert
+OPERADORES
+- ✅ MATEMÁTICOS / ARITMÉTICOS
+- ✅ DE COMPARACIÓN
+- ✅ COMPUESTOS
+CONTROL DE FLUJOS
+- ✅ CONDICIONALES IF / ELSE IF / ELSE
+- ✅ LÓGICOS - ejemplos boliche y tarj sube
+- OPERADORES TERNARIOS - abreviación del if-else
 */
 
 /* 
-luego         - TIPOS DE DATOS
-Luego         - DATOS
-LO MÁS BÁSICO - 0s y 1s*/
-
-
-
-/* los datos los almacenamos en VARIABLES 
-
-las variables son ESPACIOS EN MEMORIA
-Temporalmente y de manera aleatoria, el procesador elige un espacio de los millones que tiene la memoria RAM, para alojar un dato, con un nombre específico.
-
-Memoria RAM - MEMORIA DE ACCESO ALEATORIO - Guardar info. temporalmente
+METODOS DE JS 
+    - console.log(variable) - visualizar en consola alguna variable.
+    - alert(variable) - visualizar en el viewport a modo de ventana emergente alguna variable.
+    - confirm(variable) - similar al alert, pero con mayor control de la resp del user
+    - prompt("Ingrese su nombre")
+        espacios (vent emerg) donde el usuario puede ingresar información (SIEMPRE VA A SER STRING POR DEFECTO)
 */
 
-// 1. declaramos nuestra variable (let)
-//let edad = 30
-// elegir al azar un locker y pegarle la etiqueta "edad" en la puerta.
-// PERO NO GUARDÉ NINGÚN DATO. SOLO DEJÉ EL ESPACIO LISTO PARA SER USADO
 
-// 2. Le asignamos un valor (un dato) a esa variable (=)
-// "la variable edad es igual a el dato 30"
-// guardar el dato 30 en el locker previamente etiquetado como "edad"
-//console.log(edad)
-//edad = 45 // sobreescribir el valor de la variable (pisé el valor anterior)
-// console.log(edad)
+// DESAFIO 2 APLICANDO PROMPT Y ALERT
+/* let nombreProducto = prompt("Ingrese el nombre del producto")
+let precioUnitario = prompt("Ingrese el precio del producto")
+let cantDeseada = prompt("Ingrese la cantidad de unidades a comprar")
+let precioFinal = precioUnitario * cantDeseada
+alert(`Vas a comprar ${cantDeseada} ${nombreProducto}s`)
+alert(`El precio final es de $${precioFinal}`) */
+
+// alt gr
+
+
+/* los operadores son SIMBOLOS que tiene una función asociada
+
+esta función se trabaja con los datos que tienen adelante, atrás o ambas
+
+
+10 + 5  => 15
+operador: + , suma
+lo que tiene atras y adelante : los numeros que va a sumar
+
+*/
+
+// OPERADORES - 3 TIPOS
+
+/* let n1 = 20
+let n2 = 20 */
+
+// 1. MATEMÁTICOS - devuelven datos NUMÉRICOS
+ /*    console.log(n1 +  n2) // (+)  SUMA 
+    console.log(n1 -  n2) // (-)  RESTA
+    console.log(n1 *  n2) // (*)  MULTIPLICACIÓN
+    console.log(n1 /  n2) // (/)  DIVISIÓN
+    console.log(n1 %  n2) // (%)  RESTO DE UNA DIVISIÓN
+    console.log(n1 ** n2)  */// (**) POTENCIA
+// 2. Op. de Comparación - DEVUELVEN BOOLEANO 
+    // 2.a. me sirven también para String
+        /* console.log(n1 == n2) // == | Igual que. solo compara el valor, ignora el tipo de dato
+        console.log(n1 === n2) // === | ESTRICTAMENTE igual que. compara el valor y tipo de dato
+        console.log(n1 != n2) // !=   | DISTINTO DE 
+        console.log(n1 !== n2) */ // !== | ESTRICTAMENTE DISTINTO DE 
+    // 2.b. solo funcionan con datos numéricos
+    /* console.log("-------------------------")
+        console.log(n1 > n2)  // >  Mayor que
+        console.log(n1 < n2)  // <  Menor que
+        console.log(n1 >= n2) // >= Mayor o igual que
+        console.log(n1 <= n2)  */// <= Menor o igual que
+// 3. Op. Compuestos - Operación matemática + Asignar resultado
+    /* console.log(n1 +=  n2) // (+=)  SUMA y asignación
+    console.log(n1 -=  n2) // (-=)  RESTA y asignación
+    console.log(n1 *=  n2) // (*=)  MULTIPLICACIÓN y asignación
+    console.log(n1 /=  n2) // (/=)  DIVISIÓN y asignación
+    console.log(n1 %=  n2) // (%=)  RESTO DE UNA DIVISIÓN y asignación
+    console.log(n1 **= n2) */ // (**=) POTENCIA y asignación
+    // ejemplo
+    /* let numero1 = 10
+    console.log(numero1)
+    numero1 = numero1 + 2
+    numero1 += 2
+    console.log(numero1) */
+// 4. Op. Lógicos - se suelen usar para agrupar condiciones
+    // && AND - Conjunción
+        // La condición es TRUE solo cuando todas sus partes son TRUE
+        /* 
+            T + T = T 
+            T + F = F
+            F + T = F
+            F + F = F
+        */
+    // || OR  - Disyunción
+        // La condición es TRUE si por lo menos una de sus partes es TRUE
+        /* 
+            T + T = T 
+            T + F = T
+            F + T = T
+            F + F = F
+        */
+    // !  NOT - Negación
+        /* SI TENEMOS ALGO QUE ES TRUE, LE PONEMOS ! Y SE HACE FALSE 
+            let esEstudiante = true
+            console.log(esEstudiante) - true
+            console.log(!esEstudiante) - false (not es estudiante)
+        */
+
+// EJEMPLO AND - entrada boliche - 2 condiciones - +18 y DNI en mano
+
+// BOLICHE - +18 - 2AM
+// MATINEE - 14-17 - 7PM - 2da opción, se ejecuta cuando la 1ra falló
+
+let edadUser = 26
+let dniEnMano = false
+
+const verifEdad = () =>{
+    if (edadUser >= 18){
+        console.log("Podes pasar al BOLICHE")
+    } else if (edadUser >= 14 && edadUser <= 17 ){
+        console.log("Podes pasar a la MATINEE")
+    } else {
+        console.log("No Podes pasar a nada")
+    }
+}
+dniEnMano ? verifEdad() : console.log("NO TENES DNI, COMO CHEQUEO TU EDAD?")
+
+
+// EJEMPLO OR - SUBE con descuento
+// estudiantes o jubilados
+
+let esEstudiante = false
+let esJubilado = false
+
+/* if (esEstudiante || esJubilado){
+    console.log("Podes acceder al descuento")
+}else{
+    console.log("No aplicas al descuento en la SUBE")
+} */
+
+
+// CONDICIONALES -----------------------------------------------
 
 /* 
-TANTO VARIABLES COMO CONSTANTES SON PARA GUARDAR UN DATO EN UN ESPACIO EN MEMORIA. 
 
-LA DIFERENCIA ENTRE ELLAS ES QUE
-- let - VARIABLES - Mutables - puede cambiar el contenido de la variable
-- const - CONSTANTES - Inmutables - NO puede cambiar el contenido de la constante
+mayor de edad para entrar a un boliche
 
-*/
+si tu edad es mayor o igual a 18...
+    - podes pasar
+sino... (es decir, si es falso)
+    - no podes pasar
 
-
-//const precio = 1500 // declaré y asigné el valor en una misma linea (2x1)
-//console.log(precio)
-// precio = 3500 // NO SE PUEDE, TIRA ERROR porque es constante
-// console.log(precio)
-
-/* 
-TIPOS DE DATOS primitivos (los más básicos)
-- Numéricos
-    - int (entero) - numero sin coma (20, 3000, -13)
-    - float (con decimal) - numero CON coma (20.5, 3000.23, -13.30)
-- Boolean
-    - true 1 si verdadero
-    - false 0 no falso
-- String 
-    - cadena (unión) de CARACTERES.
-    - sirve para armar mensajes
-*/
-let nombre = `Pedro` // ej. String
-let edad = 20
-let curso = 'PHP'               // ej. int
-// let precio = 4000.50        // ej. float
-// let esEstudiante = true     // ej. boolean
-
-// forma tradicional - concatenar 3 strings con  3 variables
-/* console.log(
-"Hola "+nombre+", tenes "+edad+" años y sos parte del curso de "+curso
-) */
-// forma actual - plantillas literales - usar 1 solo string e insertarle variables donde las necesitamos.
-console.log(
-`Hola ${nombre}, tenes ${edad} años y sos parte del curso de ${curso}`
-)
-
-// Hola (nombre) tenes (edad) años y sos parte del curso de (curso)
-
-
-// es estudiante? si / no
-// camelCase - estoEsUnEjemploDeCamelCase
-    // 1er palabra toda en minuscula
-    // 2da palabra en adelante - 1er letra mayus, resto minus
-    // todo junto, sin espacios
-
-
-// @ - alt 64 - codigo ASCII del caracter
-
-// comillas invertidas `` alt gr + }
-
-
-/* 
-OPERADOR +
-DISTINTO COMPORTAMIENTO DE ACUERDO A LOS DATOS QUE OPERA
-
-numero + numero | 10     + 5       = 15          | suma
-string + numero | "hola" + 10      = "hola10"    | concatena
-numero + string | 10     + "mundo" = "10mundo"   | concatena
-string + string | "hola" + "mundo" = "holamundo" | concatena
+CONDICIÓN A EVALUAR - true 
+- Bloque de código a ejecutar si la condición es TRUE
 
 */
 
-console.log(10+4) // suma SOLAMENTE CUANDO ESTAMOS ENTRE 2 NUM
-console.log("hola"+" "+"mundo") 
-console.log("hola"+10)
-console.log(10+"mundo")
+
+// CONDICIONAL SIMPLE - SINTAXIS TRADICIONAL
+/* if (esEstudiante || esJubilado){
+    console.log("Podes acceder al descuento")
+}else{
+    console.log("No aplicas al descuento en la SUBE")
+} */
+
+// CONDICIONAL SIMPLE - OPERADOR TERNARIO
+esEstudiante || esJubilado ?
+    console.log("T - Podes acceder al descuento")
+:
+    console.log("T - No aplicas al descuento en la SUBE")
 
 
-console.log(10-4)
 
-/* tipos de operadores que vemos la clase que viene
-- op. aritméticos o matemáticos
-- op. de comparación
-- op. lógicos
 
+
+// OPERADOR TERNARIO - ABREVIACIÓN PARA LOS COND. SIMPLES. - ES6
+
+/*
+    estructura de un if-else tradicional
+    
+    if(condicion){
+        valor si es true
+    }else{
+        valor si es false
+    }
+
+    Estructura de un operador ternario
+
+    condicion ? valor si es true : valor si es false
 */
 
-let numero1 = 51
-let numero2 = "51"
 
-console.log(numero1 == numero2)
-// ¿che, 50 y "50" son iguales?
-
-console.log(numero1 === numero2)
-// ¿che, 50 y "50" son ESTRICTAMENTE iguales?
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+rifEdad = () =>{
+    if (edadUser >= 18){
+        console.log("Podes pasar al BOLICHE")
+    } else if (edadUser >= 14 && edadUser <= 17 ){
+        console.log("Podes pasar a la MATINEE")
+    } else {
+        console.log("No Podes pasar a nada")
+    }
+}
